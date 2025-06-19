6b0ecb2b5d3324b1b7848bf3f9d541558e4d5119
Rodion <oberemkoro@yandex.ru>
2025-06-19 15:38:16 +0300

    1. Сделал историю посещений неограниченной по размеру. 2. Избавился от повторных просмотров в истории. Если какую-либо задачу посещали несколько раз, то в истории должен остаться только её последний просмотр. Предыдущий должен быть удалён. 3. Покрыл тестами: 	-. Скорректировал предыдущие тесты под изменившийся алгоритм хранения версий задачи. 	-. Проверил, что встроенный связный список версий, а также операции добавления и удаления работают корректно. 	-. Покрыл тестами ещё несколько функций менеджера задач, которые реализовал в предыдущей версии. 4. Дополнительно реализовал в main пользовательский сценарий: 	- Создал две задачи, эпик с тремя подзадачами и эпик без подзадач. 	- Запросил созданные задачи несколько раз в разном порядке. 	- После каждого запроса вывел историю и убедился, что в ней нет повторов. 	- Удалил задачу, которая есть в истории, и проверил, что при печати она не будет выводиться. 	- Удалил эпик с тремя подзадачами и убедился, что из истории удалился как сам эпик, так и все его подзадачи.

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -7,17 +7,25 @@ import java.util.List;
 
 public interface TaskManager {
     Task createTask(Task task);
+
     Task getTask(int id);
+
     Collection<Task> getAllTasks();
+
     void deleteTask(int id);
+
     void deleteAllTasks();
 
     Epic createEpic(Epic epic);
+
     Collection<Epic> getAllEpics();
+
     void deleteEpic(int id);
 
     Subtask createSubtask(Subtask subtask);
+
     Collection<Subtask> getAllSubtasks();
+
     List<Subtask> getSubtasksByEpic(Epic epic);
 
     List<Task> getHistory();
